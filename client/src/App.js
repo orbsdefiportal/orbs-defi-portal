4a9eb87a30bd656a3a3f534221ab011239ede6a9
Slabunov <d.slabunov.dev@gmail.com>
2021-01-19 13:12:06 +0300

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,28 @@ const App = () => {
   const [isOverflow, setIsOverflow] = useState(false);
 
   return (
-    <div
-      className="App"
-      style={{
-        overflow: `${isLoading === false || isOverflow ? "hidden" : "auto"}`,
-        maxHeight: `${isLoading === false || isOverflow ? "100vh" : "100%"}`,
-      }}
-    >
-      <div className={isOverflow ? "overflow" : ""}></div>
-      <div
-        className="lds-loader"
-        style={{ display: `${isLoading === false ? "flex" : "none"}` }}
-      >
-        <span className="loaderSpan">Loading data ...</span>
-      </div>
-      <div>
-        <Header setIsOverflow={setIsOverflow} />
-        <Main setIsLoading={setIsLoading} />
-        <Subscribe />
-        <Footer />
-      </div>
-    </div>
+    <div>dfihgfh</div>
+    // <div
+    //   className="App"
+    //   style={{
+    //     overflow: `${isLoading === false || isOverflow ? "hidden" : "auto"}`,
+    //     maxHeight: `${isLoading === false || isOverflow ? "100vh" : "100%"}`,
+    //   }}
+    // >
+    //   <div className={isOverflow ? "overflow" : ""}></div>
+    //   <div
+    //     className="lds-loader"
+    //     style={{ display: `${isLoading === false ? "flex" : "none"}` }}
+    //   >
+    //     <span className="loaderSpan">Loading data ...</span>
+    //   </div>
+    //   <div>
+    //     <Header setIsOverflow={setIsOverflow} />
+    //     <Main setIsLoading={setIsLoading} />
+    //     <Subscribe />
+    //     <Footer />
+    //   </div>
+    // </div>
   );
 };
 
